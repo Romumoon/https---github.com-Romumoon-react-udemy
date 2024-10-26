e2b076aaa523559a8a82fca2468f1ec894fa8433
Romumoon <romu.pastore@gmail.com>
2024-10-26 18:08:28 -0300

    creating components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { createRoot } from 'react-dom/client';
+import './index.css'
+import Primeiro from './components/basicos/Primeiro';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
-root.render("Hello World!")
+root.render(
+    <div><Primeiro></Primeiro></div>
+)
