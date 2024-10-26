4a9fd30aa35e21fef910026d7ba95618d26a6dc3
Romumoon <romu.pastore@gmail.com>
2024-10-26 18:33:49 -0300

    praticing components with props

---
+++ src/components/basicos/ComParametro.jsx
@@ -0,0 +1,9 @@
+export default function ComParametro(props) {
+    const status = props.nota >= 7 ? "Aprovado" : "Recuperação"
+    return (
+        <div>
+            <h2>{props.titulo}</h2>
+            <h3>{props.aluno} tem nota {props.nota} e está {status}!</h3>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import { createRoot } from 'react-dom/client';
 import './index.css'
 import Primeiro from './components/basicos/Primeiro';
+import ComParametro from './components/basicos/ComParametro';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
 root.render(
-    <div><Primeiro></Primeiro></div>
+    <div>
+        <Primeiro/>
+        <ComParametro 
+            titulo="Situação do Aluno" 
+            aluno="Rômulo"
+            nota={5.5}/>
+    </div>
 )
